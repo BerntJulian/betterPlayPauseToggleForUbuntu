7aaa324ba38e972ffdfb29e10cc2c33206a2f50b
BerntJulian <42943928+BerntJulian@users.noreply.github.com>
2020-08-16 18:13:31 +0200

    Update betterSpotifyToggle.sh

--- betterSpotifyToggle.sh
+++ betterSpotifyToggle.sh
@@ -6,26 +6,27 @@
 
 #TODO: move spotify at the bottom of the window manager focus
 
+#Used for getting the windowfocus back to what program the original focus was. 
+windowId=$(xdotool getactivewindow)
+
 #Check if spotify is not running, if not open it.
-if ! ps --no-headers -C spotify -o args,state; then
-    
-    #Used for getting the windowfocus back to what program the original focus was. 
-    windowId=$(xdotool getactivewindow)
-    
+if ! ps --no-headers -C spotify -o args,state; then    
     (spotify &)
     sleep 0.5;
-
-    #Press play 
-    dbus-send --print-reply --dest=org.mpris.MediaPlayer2.spotify /org/mpris/MediaPlayer2 org.mpris.MediaPlayer2.Player.PlayPause
-    sleep 0.5;
-
-    #If spotify now are playing music then change the focus back to the origial window.
-    if pacmd list-sink-inputs | grep spotify; then
-        xdotool windowfocus $windowId
-        xdotool windowactivate $windowId
-    fi
-
-#If spotify are running, toggle play/pause
-else
-    dbus-send --print-reply --dest=org.mpris.MediaPlayer2.spotify /org/mpris/MediaPlayer2 org.mpris.MediaPlayer2.Player.PlayPause
 fi
+
+
+#Press play 
+dbus-send --print-reply --dest=org.mpris.MediaPlayer2.spotify /org/mpris/MediaPlayer2 org.mpris.MediaPlayer2.Player.PlayPause
+sleep 0.5;
+
+
+#If spotify not are playing change the window that should be in focus to Spotify.
+if ! pacmd list-sink-inputs | grep spotify; then
+    pid=$(pgrep spotify | head -n 1)                  
+    windowId=$(xdotool search --pid "$pid" | tail -2) #-2 magic
+fi
+
+#Bring either original window or spotify in focus.
+xdotool windowfocus $windowId
+xdotool windowactivate $windowId
