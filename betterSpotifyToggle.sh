82f57a16c24196ffa34508f3a1743b50e260a085
BerntJulian <42943928+BerntJulian@users.noreply.github.com>
2020-10-25 21:30:03 +0100

    Update betterSpotifyToggle.sh

--- betterSpotifyToggle.sh
+++ betterSpotifyToggle.sh
@@ -2,7 +2,7 @@
 
 #map this script to Custom Shortcuts  at the bottom under Settings->Devices->Keyboard
 #Name:      w/e
-#Command:   sh /home/username/bin/betterSpotifyToggle   //tilde is not supported!
+#Command:   sh /home/username/bin/betterSpotifyToggle.sh   //tilde is not supported!
 
 #TODO: move spotify at the bottom of the window manager focus
 
@@ -12,15 +12,17 @@ windowId=$(xdotool getactivewindow)
 #Check if spotify is not running, if not open it.
 if ! ps --no-headers -C spotify -o args,state; then    
     (spotify &)
-    sleep 0.5;
+    sleep 0.5
 fi
 
+pressPlayFor=5
+SECONDS=0
 #Press play until soptify is open
-while ! dbus-send --print-reply --dest=org.mpris.MediaPlayer2.spotify /org/mpris/MediaPlayer2 org.mpris.MediaPlayer2.Player.PlayPause
+while ! dbus-send --print-reply --dest=org.mpris.MediaPlayer2.spotify /org/mpris/MediaPlayer2 org.mpris.MediaPlayer2.Player.PlayPause && (( SECONDS < pressPlayFor ))
 do
-    :
+    sleep 0.2
 done
-sleep 0.5;
+sleep 0.5
 
 #If spotify does not play music after play is pressed, keep spotify in focus.
 if ! pacmd list-sink-inputs | grep spotify; then
