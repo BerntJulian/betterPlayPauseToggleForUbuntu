ec278020152a410ae373397d923b610340221fd6
BerntJulian <42943928+BerntJulian@users.noreply.github.com>
2021-04-28 15:57:36 +0200

    Update betterSpotifyToggle.sh

--- betterSpotifyToggle.sh
+++ betterSpotifyToggle.sh
@@ -3,21 +3,31 @@
 #map this script to Custom Shortcuts at the bottom under Settings->Devices->Keyboard
 #Name:      w/e
 #Command:   /home/{username}/bin/betterSpotifyToggle.sh   //tilde is not supported!
-#TODO: move spotify at the bottom of the window manager focus
 
 #Check if spotify is not running, if not open it.
 if ! ps --no-headers -C spotify -o args,state; then
     windowId=$(xdotool getactivewindow)
-    SECONDS=0
-    timeForSpotifyToOpen=5
+
+    SECONDS=0; timeForSpotifyToOpen=7
     (spotify &) 
-    while (( SECONDS < timeForSpotifyToOpen )); do pidof spotify && { sleep 1;break; } done
+
+    while (( SECONDS < timeForSpotifyToOpen )); do
+        wmctrl -l | cut -d" "  -f5- | grep -P "\bSpotify( Premium)?$" && break
+    done
+    (( SECONDS >= timeForSpotifyToOpen )) && notify-send "Could not open spotify" && exit
+
+    spotifyWid=$(wmctrl -l | grep -P "\bSpotify( Premium)?$" | cut -f 1 -d " ")
+    xdotool windowminimize $spotifyWid
+    
+    sleep 1
+    
     xdotool windowfocus $windowId
     xdotool windowactivate $windowId
 fi
 
 dbus-send --print-reply --dest=org.mpris.MediaPlayer2.spotify /org/mpris/MediaPlayer2 org.mpris.MediaPlayer2.Player.PlayPause
 sleep 0.3
+
 pacmd list-sink-inputs | grep spotify && exit
 
 spotifyWid=$(wmctrl -l | grep -P "\bSpotify( Premium)?$" | cut -f 1 -d " ")
